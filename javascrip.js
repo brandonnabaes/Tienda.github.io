78133cad9ea3c88ea06bd0ae40f380de89ebb940
brandon nabaes <brandon.nabaes@gmail.com>
2023-04-17 21:39:04 -0300

    firs

---
+++ javascrip.js
@@ -0,0 +1,26 @@
+// Obtenemos los botones por su ID y agregamos un controlador de eventos al hacer clic
+document.getElementById("btn1").addEventListener("click", function() {
+    // Aquí puedes agregar la funcionalidad que desees al hacer clic en el botón
+    alert("Has comprado la Grafica 1");
+  });
+  
+  document.getElementById("btn2").addEventListener("click", function() {
+    alert("Has comprado la Grafica 2");
+  });
+  
+  document.getElementById("btn3").addEventListener("click", function() {
+    alert("Has comprado la Grafica 3");
+  });
+  
+  document.getElementById("btn4").addEventListener("click", function() {
+    alert("Has comprado la Grafica 4");
+  });
+  
+  document.getElementById("btn5").addEventListener("click", function() {
+    alert("Has comprado la Grafica 5");
+  });
+  
+  document.getElementById("btn6").addEventListener("click", function() {
+    alert("Has comprado la Grafica 6");
+  });
+  
